f59edffc8b248d6b082e9b8f3ea3bc12f8f97a73
wen <huangwenjimmy@163.com>
2017-04-21 13:50:51 +0800

    bean.evel add

---
+++ sml-status/src/main/java/org/hw/sml/status/rs/SmlRsResource.java
@@ -0,0 +1,19 @@
+package org.hw.sml.status.rs;
+
+import org.hw.sml.rest.annotation.SmlResource;
+import org.hw.sml.support.ioc.BeanHelper;
+import org.hw.sml.support.ioc.annotation.Bean;
+@Bean
+@SmlResource("sml")
+public class SmlRsResource {
+	@SmlResource("cmd")
+	public Object evel(String elp){
+		try {
+			return BeanHelper.evelV(elp);
+		} catch (IllegalArgumentException e) {
+			return e.toString();
+		} catch (IllegalAccessException e) {
+			return e.toString();
+		}
+	}
+}
